843d4d65cc6ce866543b094edfd899d19569453d
Jhonatan Serafim <jhonatan.serafim@triersistemas.com.br>
2015-01-14 14:29:50 -0200

    Incluindo o botão de login do google+

--- src/main/webapp/script/app.js
+++ src/main/webapp/script/app.js
@@ -1,15 +1,18 @@
 'use strict';
 
-var app = angular.module('App', [ 'ngRoute', 'pascalprecht.translate', 'tmh.dynamicLocale', 'currencyMask' ]);
+var app = angular.module('App', [ 'ngRoute', 'pascalprecht.translate', 'tmh.dynamicLocale', 'currencyMask', 'directive.g+signin' ]);
 
 app.run(function($rootScope, $translate, tmhDynamicLocale) {
 	$rootScope.dataHoje = new Date();
+	$rootScope.key = 'pt-br';
 	
 	$rootScope.mudarLinguagem = function(key) {
 		$translate.use(key);
 		tmhDynamicLocale.set(key);
+		$rootScope.key = key;
 	};
-	$rootScope.mudarLinguagem('pt-br');
+	
+	$rootScope.mudarLinguagem($rootScope.key);
 	
 	$rootScope.addMsgSucesso = function(msgSucesso) {
 		$rootScope.msgSucesso = msgSucesso;
@@ -21,5 +24,11 @@ app.run(function($rootScope, $translate, tmhDynamicLocale) {
 		}, 4000);
 	};
 	
-	$rootScope.valor = 51122.34;
+	$rootScope.$on('event:google-plus-signin-success', function(event, authResult) {
+		console.log('Signed in!');
+		console.log(authResult);
+	});
+	$rootScope.$on('event:google-plus-signin-failure', function(event, authResult) {
+		console.log('Not signed into Google Plus.');
+	});
 });
